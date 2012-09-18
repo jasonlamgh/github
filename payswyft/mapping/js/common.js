34a5a9061dd9165dc3619ac0e98f398dd9f233ad
Jason Lam <jason.lam@live.com>
2012-09-18 15:18:20 +1000

    Commit

--- payswyft/mapping/js/common.js
+++ payswyft/mapping/js/common.js
@@ -2,37 +2,115 @@ $(document).ready(function()
 {
 	jQuery.Common 			= function(){};	
 	jQuery.Common.Check		= function(){};
+	jQuery.Common.CSV		= function(){};
 	jQuery.Common.Logger	= function(){};
 	jQuery.Common.String 	= function(){};
 	jQuery.Common.Validator = function(){};
 	
+	/*
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
+	 */
 	jQuery.Common.Check.FileReader = function(){
 		if (window.File && window.FileReader && window.FileList && window.Blob) { return true; }
 		return false;
 	};
 	
+	/*
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
+	 */
 	jQuery.Common.Check.Flash = function(value) {
 		var p = swfobject.getFlashPlayerVersion(); // returns a JavaScript object
 		var v = p.major; // access the major, minor and release version numbers via their respective properties		
 		if(v >= value) { return true; }		
 		return false;
 	};
+	
+	/*
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
+	 */
+	jQuery.Common.CSV.Clean = function(value) {
+		var regex = '';
+		var csv = value;
 
+		//("?(?:"|\$|-)*(\d)*(,\d{3}))
+
+		// comma(s) not within double quotes	
+		regex = /([^"]+?),([^"]+)/g;
+		while (m = regex.exec(csv)) {
+		    var start = m.index;
+		    var end = m.index + m[0].length;
+		    csv = jQuery.Common.String.ReplaceByRangeIndex(csv, "", start, end);
+		}
+		
+		// replace $, "
+		regex = /[\$"]/g;
+		csv = csv.replace(regex, '');
+				
+		return csv;
+	}
+	
+	/*
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
+	 */
+	jQuery.Common.CSV.IsValid = function(value) {
+		var result 	= true;
+		var regex	= '';		
+				 
+		// :TODO: throw custom error		
+		regex = /([^"]+?),([^"]+)/gm;		 
+		if(value.search(regex)!= -1)
+			return false; 
+		 
+		return result;
+	 } 
 			
 	/*
-	 *	<syntax>jQuery.Common.String.IsEmpty</syntax>
-	 *	<parameters>value The initial value of the String object</parameters>
-	 *	<description>returns true or false if the string value passed in empty, undefined, null or blank</description>
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
 	 */
 	jQuery.Common.String.IsEmpty = function(value) {
 		return (!value || /^\s*$/.test(value));	
 	};
 	
+	/*
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
+	 */
+	jQuery.Common.String.ReplaceByRangeIndex = function(string, replace, start, end) {
+	    var str = string;    
+	    for (var i = start; i < end; i++) {
+	        if (str.charAt(i) == ",") {
+	            str = str.slice(0, (i)) + replace + str.slice((i + 1), str.length);
+	        }
+	    }
+	    return str;
+	}
+	
+	/*
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
+	 */
 	jQuery.Common.Logger.Console = function(value) {
 		var isDebug = DEBUG ? true : false;
 		if(isDebug){ console.log(value); }
 	}
 	
+	/*
+	 *	<syntax></syntax>
+	 *	<parameters></parameters>
+	 *	<description></description>
+	 */
 	jQuery.Common.Validator.ErrorMessage = function(value) {
 		var obj = {}
 		var msg = {
